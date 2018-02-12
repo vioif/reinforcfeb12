63bae936be2dbc0b1423d5cfd0c78f9754e2fc22
vioif <ireneaflores3@gmail.com>
2018-02-12 13:41:06 -0500

    completed exercise

---
+++ exercise.js
@@ -0,0 +1,12 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+for(var i = 0; i < items.length; i++){
+  console.log((i + 1) + ': ' + items[i]);
+  // console.log("Todo:");
+  // console.log(i);
+}
+
+// Todo:
+// 1: purchase domain
+// 2: install SSL certificate
+// 3: deploy site
+// * 4: celebrate success
